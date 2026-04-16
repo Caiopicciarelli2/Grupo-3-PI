d4145093c57e414f94f33110a6d889064f7c3f5b
caiopicciarelli2 <caio.psilva@sptech.school>
2026-04-16 12:34:10 -0300

    Atualizando lógica do login e cadastro e fazendo mudanças no css a pedido do cliente

--- Cadastro_Login/assets/js/script.js
+++ Cadastro_Login/assets/js/script.js
@@ -1,79 +1,56 @@
-// verificação login 
-function cadastro_verificacao(){
-    // entrada 
-    let nome = (ipt_nome.value);
-    let email = (ipt_email.value);
-    let telefone = (ipt_telefone.value);
-    let senha = (ipt_senha.value);
-    let senha_confirmacao = (ipt_senha_confirmacao.value);
-    let boolean_email = false;
-    let erro_cadastro = false;
-    let sucesso_cadastro = false;
-    
-    // código do erro:
-    erro_status.innerHTML = ``;
-    // código de sucesso
-    sucesso.innerHTML = ``;
+// login
 
-    // elementos HTML
-    let senha_erro = document.querySelector('.info-row-description');
-    let content_erro = document.querySelector('.container-aviso');
-    let content_sucesso = document.querySelector('.container-sucesso');
+let listas_user = [
+    'Caio@flowtrak.com',
+    'Emanuelly@flowtrak.com',
+    'Vitor@flowtrak.com',
+    'Victor@flowtrak.com',
+    'Karina@flowtrak.com',
+    'Isaac@flowtrak.com'
+];
+let listas_senha = ['caio1234', 'ema1234', 'vitor1234', 'victor1234', 'karina1234', 'isaac1234'];
+let contador_tentativas = 3;
 
-    if(email.includes("@") && email.includes(".")){
-        boolean_email = true;
+function logar() {
+    // entradas
+    let nome_login = (ipt_email_login.value);
+    let senha_login = (ipt_senha_login.value);
+    // reset tentativas texto
+    erro_tentativas.innerHTML = ``;
+    login_sucesso.innerHTML = ``;
+
+    // contador de comparação 
+    let contador_user = 0;
+
+    // verificação de preenchimento dos campos
+    if (nome_login != "" && senha_login != "") {
+        // verifica se o usuario ainda tem tentativas
+        if (contador_tentativas > 0) {
+            // verifica se o user_name que o usuario colocou no input está na lista de usuarios
+            if (listas_user.includes(nome_login)) {
+                // procura em qual vetor da lista esse usuario está, para simular um BD com IDs, sendo 
+                // o user e a senha no mesmo campo das listas (listas_user) e (listas_senhas)
+                // para ele verificar nesse mesmo vetor na outra lista
+                for (let i = 0; i < listas_user.length; i++) {
+                    if (nome_login == listas_user[i]) {
+                        contador_user = i; // atribui o número do vetor a uma variavel para comparar depois 
+                    }
+                }
+
+                if (senha_login == listas_senha[contador_user]) { // usa essa variavel
+                    login_sucesso.innerHTML = `sucesso`; // modal sucesso
+                } else {
+                    contador_tentativas --; 
+                    alert("Sua senha está incorreta");
+                }
+
+            } else {
+                alert("Este usuário não existe!")
+            }
+        } else {
+            erro_tentativas.innerHTML = `erro`; // modal_error
+        }
+    } else {
+        alert("Preencha os campos corretamente!")
     }
-
-    // verificação nome mínimo 3
-    if(nome == "" || email == "" || telefone == "" || senha == "" || senha_confirmacao == ""){
-        content_erro.style.display = 'flex';
-        erro_status.innerHTML = `Preencha os campos corretamentes!`;
-        erro_cadastro = true;
-    }else if(nome.length <= 3){
-        content_erro.style.display = 'flex';
-        erro_status.innerHTML = `Seu nome deve ter pelo menos 4 caracteres!`;
-        erro_cadastro = true;
-    }else if(!boolean_email){
-        content_erro.style.display = 'flex';
-        erro_status.innerHTML = `Seu email deve conter "@" e "."`;
-        erro_cadastro = true;
-    }else if(telefone.length < 10){
-        content_erro.style.display = 'flex';
-        erro_status.innerHTML = `Seu email deve ter no mínimo 11 dígitos`;
-        erro_cadastro = true;
-    }else if(senha.length < 5){
-        senha_erro.style.display = 'flex';
-        erro_cadastro = true;
-    }else if(senha != senha_confirmacao){
-        content_erro.style.display = 'flex';
-        erro_status.innerHTML = `Suas senhas devem coincidir!`;
-        erro_cadastro = true;
-    }else{
-        content_sucesso.style.display = 'flex';
-        sucesso.innerHTML = `Seu login foi um sucesso!<br> Estamos te redirecionando.`;
-        sucesso_cadastro = true;
-    }
-
-    if(sucesso_cadastro){
-        setTimeout(() => {
-            location.reload();
-        }, 5000);
-    }
-};
-
-// fechar modais de aviso
-let content_erro = document.querySelector('.container-aviso');
-let content_sucesso = document.querySelector('.container-sucesso')
-let btn_fechar_sucesso = document.getElementById('btn_fechar_sucesso');
-let btn_fechar_aviso = document.getElementById('btn_fechar_aviso');
-
-btn_fechar_aviso.addEventListener('click', () =>{
-    content_erro.style.display = 'none';
-    setTimeout(() => {
-        location.reload();
-    });
-})
-
-btn_fechar_sucesso.addEventListener('click', () =>{
-    content_sucesso.style.display = 'none';
-})
+}
